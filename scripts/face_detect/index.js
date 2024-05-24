4eed5cfa19ba834b370a16e5762791e719784395
Andreas Gysin <andreas@ertdfgcvb.ch>
2024-05-24 12:54:01 +0200

    Update index.js

--- scripts/face_detect/index.js
+++ scripts/face_detect/index.js
@@ -2,27 +2,25 @@ import { tidy } from '@tensorflow/tfjs-node' // in nodejs environments tfjs-node
 import { tf as TF, detectAllFaces, version, nets, SsdMobilenetv1Options } from '@vladmandic/face-api' // use this when face-api is installed as module (majority of use cases)
 import fs from 'fs'
 import path from 'path'
+import { scanFolder, getImagePath, loadSharpImage, cropAndSaveSharpImage } from './common.js'
+
+const IMG_PATH       = getImagePath()    // percorso delle cartella delle immagini (relativo a questo script)
+const CROP_PATH      = path.join(IMG_PATH, "..", "face_crop")
+const JSON_PATH      = path.join(IMG_PATH, "..", "data_face.json") // Nome del file per il salvataggio dei dati
+const SALVA_CROP     = true              // salvare le immagini croppate?
+const CROP_SIZE      = 256              // ridimensiona crop (lasciare “null” per dimensione originale)
 
 const MIN_CONFIDENCE_THRESHOLD = 0.15
-const MAX_RESULTS = 50
+const MAX_RESULTS    = 50
 
-// Nome del file per il salvataggio dei dati
-const OUTPUT = "data_faces.json"
+run()
 
-// Percorso delle cartella delle immagini (relativo a questo script)
-const IMG_PATH = "../img_orig/"
+async function run() {
 
-// Files da ignorare:
-const FILES_DA_IGNORARE = ['.DS_Store', '.AppleDouble', '.LSOverride']
+	const MODEL_PATH = './model/'
+	if (SALVA_CROP && !fs.existsSync(CROP_PATH)) fs.mkdirSync(CROP_PATH)
 
-// Elenco di tutti i files nella cartella PATH
-const files = fs.readdirSync(IMG_PATH).filter( e => FILES_DA_IGNORARE.indexOf(e) == -1)
-
-const MODEL_PATH = './model/'
-
-run(files, IMG_PATH, MODEL_PATH)
-
-async function run(files, img_path, model_path) {
+	const files = scanFolder(IMG_PATH)
 
 	await TF.setBackend('tensorflow')
 	await TF.ready()
@@ -30,11 +28,11 @@ async function run(files, img_path, model_path) {
 	console.log(`Version: TensorFlow/JS ${TF?.version_core} FaceAPI ${version} Backend: ${TF?.getBackend()}`)
 	console.log('Loading FaceAPI models')
 
-	await nets.ssdMobilenetv1.loadFromDisk(model_path)
-	await nets.ageGenderNet.loadFromDisk(model_path)
-	await nets.faceLandmark68Net.loadFromDisk(model_path)
-	await nets.faceRecognitionNet.loadFromDisk(model_path)
-	await nets.faceExpressionNet.loadFromDisk(model_path)
+	await nets.ssdMobilenetv1.loadFromDisk(MODEL_PATH)
+	await nets.ageGenderNet.loadFromDisk(MODEL_PATH)
+	await nets.faceLandmark68Net.loadFromDisk(MODEL_PATH)
+	await nets.faceRecognitionNet.loadFromDisk(MODEL_PATH)
+	await nets.faceExpressionNet.loadFromDisk(MODEL_PATH)
 	const optionsSSDMobileNet = new SsdMobilenetv1Options({ MIN_CONFIDENCE_THRESHOLD, MAX_RESULTS })
 
 	const data = []
@@ -44,11 +42,15 @@ async function run(files, img_path, model_path) {
 	const t0 = (new Date()).getTime()
 
 	for (const file of files) {
-		// if (!img.toLocaleLowerCase().endsWith('.jpg')) continue
-		const tensor = await image(path.join(img_path, file))
-		const result = await detect(tensor, optionsSSDMobileNet)
-		const Faces = result.map(getFormattedData)
 
+		const FileName      = path.parse(file).name
+		const FileExtension = path.extname(file)
+		const sharp_img     = await loadSharpImage(path.join(IMG_PATH, file))
+
+		const img_buf       = await sharp_img.image.toBuffer()
+		const tensor        = getTensor(img_buf)
+		const result        = await detect(tensor, optionsSSDMobileNet)
+		const Faces         = result.map(getFormattedData)
 
 		// print
 		console.log('File: ' + file + ', numero di volti: '+ result.length)
@@ -62,12 +64,21 @@ async function run(files, img_path, model_path) {
 		}
 
 		data.push({
-			ImageWidth    : tensor.shape[2], // TODO: invertiti?
-			ImageHeight   : tensor.shape[1],
-			FileExtension : path.extname(file),
-			FileName      : path.parse(file).name,
+			FileName,
+			FileExtension,
+			ImageWidth  : sharp_img.md.width,
+			ImageHeight : sharp_img.md.height,
 			Faces
 		})
+
+		if (SALVA_CROP) {
+			let idx = 0
+			for (const o of Faces) {
+				const output_path = path.join(CROP_PATH, FileName + "_" + idx++ + ".jpg")
+				cropAndSaveSharpImage(sharp_img.image, o.box, output_path, CROP_SIZE)
+			}
+		}
+
 		num_volti_trovati += result.length
 
 		tensor.dispose()
@@ -80,18 +91,14 @@ async function run(files, img_path, model_path) {
 	console.log("Tempo impiegato: " + (t1 - t0) + "ms")
 	console.log("Numero di immagini analizzate: " + data.length)
 	console.log("Numero di volti identificati: " + num_volti_trovati)
-	console.log("Scrivo dati nel file: " + OUTPUT + "...")
-	fs.writeFileSync(OUTPUT, JSON.stringify(data, null, 4), 'utf8')
+	console.log("Scrivo dati nel file: " + JSON_PATH + "...")
+	fs.writeFileSync(JSON_PATH, JSON.stringify(data, null, 4), 'utf8')
 	console.log("Fatto!")
 	console.log(":)")
 
 }
 
-async function image(input) {
-	console.log('Loading image:', input)
-	const buffer = fs.readFileSync(input)
-
-	if (!buffer) return {}
+function getTensor(buffer) {
 	const tensor = tidy(() => {
 		const decode = TF.node.decodeImage(buffer, 3)
 		let expand
@@ -112,12 +119,10 @@ function getFormattedData(face) {
 	const expressions = Object.entries(face.expressions).reduce((acc, val) => ((val[1] > acc[1]) ? val : acc), ['', 0])
 	return {
 		box : {
-			x1 : Math.floor(face.alignedRect._box._x),
-			y1 : Math.floor(face.alignedRect._box._y),
-			x2 : Math.floor(face.alignedRect._box._x + face.alignedRect._box._width),
-			y2 : Math.floor(face.alignedRect._box._y + face.alignedRect._box._height),
-			w  : Math.floor(face.alignedRect._box._width),
-			h  : Math.floor(face.alignedRect._box._height),
+			left   : Math.floor(face.alignedRect._box._x),
+			top    : Math.floor(face.alignedRect._box._y),
+			width  : Math.floor(face.alignedRect._box._width),
+			height : Math.floor(face.alignedRect._box._height),
 		},
 		confidence : Math.round(face.detection._score * 1000) / 10,
 		gender : face.gender,
