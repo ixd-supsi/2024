e8727d845887bf049a01375c2d022473ee91893e
Andreas Gysin <andreas@ertdfgcvb.ch>
2024-05-24 13:10:02 +0200

    Create common.js

---
+++ scripts/exif_extract/common.js
@@ -0,0 +1,45 @@
+import fs from 'fs'
+// import sharp from 'sharp'
+
+const DEFAULT_CFG_PATH_LOCATION = "../common/image_path.cfg"
+const FILES_DA_IGNORARE = ['.DS_Store', '.AppleDouble', '.LSOverride']
+
+export function getImagePath() {
+	try {
+		return fs.readFileSync(DEFAULT_CFG_PATH_LOCATION, 'utf8').trim()
+	} catch (e) {
+		console.log("Percorso immagine default non trovato/errato.")
+		console.log("Verifica il file " + DEFAULT_CFG_PATH_LOCATION)
+	}
+}
+
+export function scanFolder(path) {
+	return fs.readdirSync(path).filter( e => FILES_DA_IGNORARE.indexOf(e) == -1)
+}
+
+// export async function loadSharpImage(image_path) {
+// 	const image = sharp(image_path)
+// 	const md = await image.metadata()
+// 	return {
+// 		image, // align based on metadata
+// 		md
+//  	}
+// }
+
+// export async function cropAndSaveSharpImage(img, box, savePath, cropSize = null) {
+// 	try {
+// 		if (cropSize) {
+// 			img.clone().extract(box)
+// 			   .resize(cropSize)
+// 			   .rotate()
+// 			   .toFile(savePath)
+
+// 		} else {
+// 			img.clone().extract(box)
+// 			   .rotate()
+// 			   .toFile(savePath)
+// 		}
+// 	} catch (e) {
+// 		console.log("Errore nel file: " + savePath)
+// 	}
+// }
